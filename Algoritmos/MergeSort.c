b52c03a625da154ef6cb4ba4259af715d1b808a8
joaorobiatti <joao.robiatti@gmail.com>
2021-12-03 17:01:05 -0300

    Tentativa - Merge

--- Algoritmos/MergeSort.c
+++ Algoritmos/MergeSort.c
@@ -1,14 +1,63 @@
-void mergesort_i (int *v,int tam){
-    int b = 1;
-    while (b < tam) {
-        int p = 0;
-        while (p + b < tam) {
-            int r = p + 2*b;
-            if (r > tam) 
-                r = tam;
-            intercala (p, p+b, r, v);
-            p = p + 2*b; 
-        }
-    b = 2*b;
+#include <stdio.h>
+#include <stdlib.h>
+
+void Intercala(int *v, int p, int q, int r) {
+   int temp[r-p+1];
+
+	int i = p, j = q+1, k = 0;
+
+	while(i <= q && j <= r) {
+		if(v[i] <= v[j]) {
+			temp[k] = v[i];
+			k += 1; i += 1;
+		}
+		else {
+			temp[k] = v[j];
+			k += 1; j += 1;
+		}
+	}
+
+	while(i <= q) {
+		temp[k] = v[i];
+		k += 1; i += 1;
+	}
+
+	while(j <= r) {
+		temp[k] = v[j];
+		k += 1; j += 1;
+	}
+
+	for(i = p; i <= r; i += 1) {
+		v[i] = temp[i - p];
+	}
+}
+
+void Mergesort(int *v,int p,int r,int tam){
+   int q;
+
+   if (p < r) {  
+   q = (p+r)/2;
+   Mergesort(v, p, q,tam);
+   Mergesort(v, q+1, r,tam);
+   Intercala(v, p, q, r);
+   }else{
+      for(int i=0;i<tam;i++){
+         printf("%d ",v[i]);
+      }
+      return;
    }
+}
+
+int main(){
+   int i,inicio, fim,tam,*vet;
+   scanf("%d",&tam);
+   vet = (int *) malloc(tam * sizeof(int));
+   
+   for (i = 0; i <tam; i++) {
+    scanf ("%d",&vet[i]);
+  }
+  inicio = vet[0];
+  fim = vet[tam-1];
+  Mergesort(vet,inicio,fim,tam);
+  free(vet);
 }
