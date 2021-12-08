8c116e358b4cdefbc86a65d57e4aab8fbc453352
jausmit <jprm951@gmail.com>
2021-12-08 16:08:57 -0300

    [FEATURE] Relatorio Geral

--- Relatorio/Relatorio.c
+++ Relatorio/Relatorio.c
@@ -3,7 +3,7 @@
 void gerarRelatorioTxt(char* nome, char* condicao, int tamanhoVetor, long tempoMicroSec, long tempoSec, int comparacoes, int trocas){
 
     char novoNome[50];
-    
+
     sprintf(novoNome,"..\\Relatorio\\Resultados\\%s\\%s.txt",nome, nome);
 
     FILE* arquivo = fopen(novoNome,"a+");
@@ -42,18 +42,64 @@ void gerarRelatorioCsv(char* nome, char* condicao, int tamanhoVetor, long tempoM
 
 }
 
+void gerarRelatorioGeralTxt(char* nome, char* condicao, int tamanhoVetor, long tempoMicroSec, long tempoSec, int comparacoes, int trocas){
+
+    char novoNome[50];
+
+    sprintf(novoNome,"..\\Relatorio\\Resultados\\Geral\\Geral.txt");
+
+    FILE* arquivo = fopen(novoNome,"a+");
+
+    fprintf(arquivo,"Algoritmo:                    %s\nCondição:                     %s\nTamanho do vetor:             %d\nTempo de execução (microsec): %ld\nTempo de execução (sec):      %ld\nComparações realizadas:       %d\nTrocas realizadas:            %d\n\n", nome, condicao, tamanhoVetor, tempoMicroSec, tempoSec,comparacoes, trocas);
+
+    fclose(arquivo);
+
+}
+
+void gerarRelatorioGeralCsv(char* nome, char* condicao, int tamanhoVetor, long tempoMicroSec, long tempoSec, int comparacoes, int trocas){
+
+    char novoNome[50];
+
+    sprintf(novoNome,"..\\Relatorio\\Resultados\\Geral\\Geral.csv");
+
+    FILE* arquivo = fopen(novoNome,"r");
+
+    if(arquivo == NULL){
+
+        fclose(arquivo);
+
+        arquivo = fopen(novoNome,"w");
+
+        fprintf(arquivo,"Algoritmo,Condição,Tamanho do vetor,Tempo de execução (microsec),Tempo de execução (sec),Comparações realizadas,Trocas realizadas");
+
+    }
+        
+    fclose(arquivo);
+
+    arquivo = fopen(novoNome,"a+");
+
+    fprintf(arquivo,"\n%s,%s,%d,%ld,%ld,%d,%d", nome, condicao, tamanhoVetor, tempoMicroSec, tempoSec,comparacoes, trocas);
+
+    fclose(arquivo);
+
+}
+
 void gerarRelatorio(char* nome, char* condicao, int tamanhoVetor, long tempoMicroSec, long tempoSec, int comparacoes, int trocas){
 
     gerarRelatorioTxt(nome,condicao,tamanhoVetor,tempoMicroSec,tempoSec,comparacoes,trocas);
 
+    gerarRelatorioGeralTxt(nome,condicao,tamanhoVetor,tempoMicroSec,tempoSec,comparacoes,trocas);
+
     gerarRelatorioCsv(nome,condicao,tamanhoVetor,tempoMicroSec,tempoSec,comparacoes,trocas);
 
-}
+    gerarRelatorioGeralCsv(nome,condicao,tamanhoVetor,tempoMicroSec,tempoSec,comparacoes,trocas);
 
+}
 
 int main(void){
 
        gerarRelatorio("Merge Sort","Totalmente desordenado",10,2217,30,500,1000);
+       
        gerarRelatorio("Quick Sort","Totalmente desordenado",10,2217,30,500,1000);
 
        return 0;
