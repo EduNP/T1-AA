abf77bab91274d12ce2f99fc212f35cac955d1c9
jausmit <jprm951@gmail.com>
2021-12-08 15:33:43 -0300

    [FEATURE] Relatorio csv e txt

--- Relatorio/Relatorio.c
+++ Relatorio/Relatorio.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void gerarRelatorio(char* nome, char* condicao, int tamanhoVetor, long tempoMicroSec, long tempoSec, int comparacoes, int trocas){
+void gerarRelatorioTxt(char* nome, char* condicao, int tamanhoVetor, long tempoMicroSec, long tempoSec, int comparacoes, int trocas){
 
     char novoNome[50];
     sprintf(novoNome,"..\\Relatorio\\Resultados\\%s.txt",nome);
@@ -13,10 +13,46 @@ void gerarRelatorio(char* nome, char* condicao, int tamanhoVetor, long tempoMicr
 
 }
 
-//   int main(void){
+void gerarRelatorioCsv(char* nome, char* condicao, int tamanhoVetor, long tempoMicroSec, long tempoSec, int comparacoes, int trocas){
+
+    char novoNome[50];
+
+    sprintf(novoNome,"..\\Relatorio\\Resultados\\%s.csv",nome);
+
+    FILE* arquivo = fopen(novoNome,"r");
+
+    if(arquivo == NULL){
+
+        fclose(arquivo);
+
+        arquivo = fopen(novoNome,"w");
+
+        fprintf(arquivo,"Algoritmo,Condição,Tamanho do vetor,Tempo de execução (microsec),Tempo de execução (sec),Comparações realizadas,Trocas realizadas");
+
+    }
+        
+    fclose(arquivo);
+
+    arquivo = fopen(novoNome,"a+");
+
+    fprintf(arquivo,"\n%s,%s,%d,%ld,%ld,%d,%d", nome, condicao, tamanhoVetor, tempoMicroSec, tempoSec,comparacoes, trocas);
+
+    fclose(arquivo);
+
+}
+
+void gerarRelatorio(char* nome, char* condicao, int tamanhoVetor, long tempoMicroSec, long tempoSec, int comparacoes, int trocas){
+
+    gerarRelatorioTxt("Merge Sort","Totalmente desordenado",10,2217,30,500,1000);
+
+    gerarRelatorioCsv("Merge Sort","Totalmente desordenado",10,2217,30,500,1000);
+
+}
 
 
-//       gerarRelatorio("Merge Sort","Totalmente desordenado",10,27,30,500,1000);
+int main(void){
 
-//       return 0;
-//   }
+       gerarRelatorio("Merge Sort","Totalmente desordenado",10,2217,30,500,1000);
+
+       return 0;
+}
