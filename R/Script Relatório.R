3534069da1e68e8c09cdfb49a408d2b1c31d1d5b
Eduardo <54189084+EduNP@users.noreply.github.com>
2021-12-09 18:10:49 -0300

    [Update] R

--- R/Script Relatório.R
+++ R/Script Relatório.R
@@ -11,42 +11,41 @@ menu=function(op){
   cat("\n5 - Quick_Sort:")
   
 }
-
-#alterar opcao de acordo com o algoritmo
-op <-2     
 menu(op)
+#alterar opcao de acordo com o algoritmo
+op <-1     
+
 
 switch(op,
-       "1" = {dados<- read_csv("GitHub/T1-AA/Relatorio/Resultados/Selection Sort/Selection Sort.csv")
+       "1" = {dados <- read_csv("~/GitHub/T1-AA/Relatorio/Resultados/Selection Sort/Selection Sort.csv")
               nome<- "Selection Sort"},
        
-       "2" = {dados<- read_csv("GitHub/T1-AA/Relatorio/Resultados/Insertion Sort/Insertion Sort.csv")
+       "2" = {dados<- read_csv("~/GitHub/T1-AA/Relatorio/Resultados/Insertion Sort/Insertion Sort.csv")
               nome<- "Insertion Sort"},
        
-       "3" = {dados<- read_csv("GitHub/T1-AA/Relatorio/Resultados/Merge Sort/Merge Sort.csv")
+       "3" = {dados<- read_csv("~/GitHub/T1-AA/Relatorio/Resultados/Merge Sort/Merge Sort.csv")
               nome<- "Merge Sort"},
        
-       "4" = {dados<- read_csv("GitHub/T1-AA/Relatorio/Resultados/Heap Sort/Heap Sort.csv")
+       "4" = {dados<- read_csv("~/GitHub/T1-AA/Relatorio/Resultados/Heap Sort/Heap Sort.csv")
               nome<- "Heap Sort"},
        
-       "5" = {dados<- read_csv("GitHub/T1-AA/Relatorio/Resultados/Quick Sort Pivo Aleatorio/Quick Sort Pivo Aleatorio.csv")
-              nome<- "Quick Sort Pivo Fixo"},
+       "5" = {dados<- read_csv("~/GitHub/T1-AA/Relatorio/Resultados/Quick Sort Pivo Aleatorio/Quick Sort Pivo Aleatorio.csv")
+              nome<- "Quick Sort Pivo Aleatorio"},
        
-       "6" = {dados<- read_csv("GitHub/T1-AA/Relatorio/Resultados/Quick Sort Pivo Fixo/Quick Sort Pivo Fixo.csv")
-       nome<- "Quick Sort Pivo Aleatorio"},
+       "6" = {dados<- read_csv("~/GitHub/T1-AA/Relatorio/Resultados/Quick Sort Pivo Fixo/Quick Sort Pivo Fixo.csv")
+       nome<- "Quick Sort Pivo Fixo"},
        
        stop("opção inválida")
 )
- 
+
+dados = subset(dados, Condição == 'ordem-decrescente')
 
 plot(dados$`Tamanho do vetor`, dados$`Tempo de execução (microsec)`, 
      xlab = "Tamanho do vetor", ylab = "Tempo de execução", 
      main = nome)
 
 
-#Média:   
 library(dplyr)
-
 graph<- aggregate(dados$`Tempo de execução (microsec)`, list(dados$`Tamanho do vetor`), FUN = mean)
 #plot das médias
 plot(graph, xlab = "Tamanho do vetor", ylab = "Tempo de execução", main = bquote(.(nome) ~"- Médias"))
@@ -54,7 +53,7 @@ plot(graph, xlab = "Tamanho do vetor", ylab = "Tempo de execução", main = bquote
 #plot com comparação de O(N)
 plot(graph, xlab = "Tamanho do vetor", ylab = "Tempo de execução", main = bquote(.(nome) ~"- Comparação com O(n²)"), pch = 18, col = "gray")
 par(new = TRUE)
-curve(x^2, from = 0, to = max(graph$Group.1),axes = false, col = "red",lwd=2)
+curve(x^2, from = 0, to = max(graph$Group.1),axes = FALSE, col = "red",lwd=2, ylab = "", xlab = "")
 
 #plot do gráfico com escala detalhada
 #plot(graph$Group.1, graph$x, pch=19,xlab = "Tamanho do vetor", ylab = "Tempo de execução", xlim=c(0,250), ylim=c(0,100), main=bquote(.(nome) ~"- Médias"))
