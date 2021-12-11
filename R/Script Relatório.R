5ecbab39f45def8ab89c21d1a8aec89a7b8ccd86
Eduardo <54189084+EduNP@users.noreply.github.com>
2021-12-11 16:29:57 -0300

    [Update] R

--- R/Script Relatório.R
+++ R/Script Relatório.R
@@ -12,10 +12,11 @@ menu=function(op){
   
 }
 menu(op)
+
 #alterar opcao de acordo com o algoritmo
-op <-1     
+op <-2     
 
-
+library(readr)
 switch(op,
        "1" = {dados <- read_csv("~/GitHub/T1-AA/Relatorio/Resultados/Selection Sort/Selection Sort.csv")
               nome<- "Selection Sort"},
@@ -37,8 +38,9 @@ switch(op,
        
        stop("opção inválida")
 )
-
-dados = subset(dados, Condição == 'ordem-decrescente')
+dados = subset(dados, Condição == 'aleatorio')
+#dados = subset(dados, Condição == 'ordem-decrescente')
+#dados = subset(dados, Condição == 'ordem-crescente')
 
 plot(dados$`Tamanho do vetor`, dados$`Tempo de execução (microsec)`, 
      xlab = "Tamanho do vetor", ylab = "Tempo de execução", 
@@ -51,9 +53,14 @@ graph<- aggregate(dados$`Tempo de execução (microsec)`, list(dados$`Tamanho do v
 plot(graph, xlab = "Tamanho do vetor", ylab = "Tempo de execução", main = bquote(.(nome) ~"- Médias"))
 
 #plot com comparação de O(N)
-plot(graph, xlab = "Tamanho do vetor", ylab = "Tempo de execução", main = bquote(.(nome) ~"- Comparação com O(n²)"), pch = 18, col = "gray")
+plot(graph, xlab = "Tamanho do vetor", ylab = "Tempo de execução", main = bquote(.(nome) ~"- Comparação"), pch = 18, col = "gray",xlim=c(0,10000),ylim=c(0,60000))
+
 par(new = TRUE)
-curve(x^2, from = 0, to = max(graph$Group.1),axes = FALSE, col = "red",lwd=2, ylab = "", xlab = "")
+curve(x^2, from = 0, to = 10000, axes = FALSE, col = "red", ylab = "", xlab = "",lwd=2,xlim=c(0,10000),ylim=c(0,60000))
+
+par(new = TRUE)
+curve(x^1, from = 0, to = 10000, axes = FALSE,lwd=2, ylab = "", xlab = "" ,col = "green",xlim=c(0,10000),ylim=c(0,60000))
+
 
 #plot do gráfico com escala detalhada
 #plot(graph$Group.1, graph$x, pch=19,xlab = "Tamanho do vetor", ylab = "Tempo de execução", xlim=c(0,250), ylim=c(0,100), main=bquote(.(nome) ~"- Médias"))
