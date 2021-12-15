3c68168902bc33c6061084736cdca69eb0550d00
Eduardo <54189084+EduNP@users.noreply.github.com>
2021-12-15 15:19:22 -0300

    Update Script Relatório.R

--- R/Script Relatório.R
+++ R/Script Relatório.R
@@ -1,5 +1,5 @@
 #alterar opcao de acordo com o algoritmo
-op <-2     
+op <-1     
 
 library(readr)
 switch(op,
@@ -24,34 +24,36 @@ switch(op,
        stop("opï¿½ï¿½o invï¿½lida")
 )
 #somar total de segundo*1000000 + Microsegundos
-dados$'Tempo total' = dados$`Tempo de execuï¿½ï¿½o (microsec)` + (1000000*dados$`Tempo de execuï¿½ï¿½o (sec)`)
+dados$'Tempo total' = dados$`Tempo de execução (microsec)` + (1000000*dados$`Tempo de execução (sec)`)
 
-#dados = subset(dados, Condiï¿½ï¿½o == 'aleatorio')
-#nome = bquote(.(nomeFile)~"- Vetores Aleatï¿½rios")
+#dados = subset(dados, Condiçãoo == 'aleatorio')
+#nome = bquote(.(nomeFile)~"- Vetores Aleatórios")
 
-dados = subset(dados, Condiï¿½ï¿½o == 'ordem-decrescente')
-nome = bquote(.(nomeFile)~"- Vetores Ordem Decrescente")
 
-#dados = subset(dados, Condiï¿½ï¿½o == 'ordem-crescente')
+#dados = subset(dados, Condição == 'ordem-decrescente')
+#nome = bquote(.(nomeFile)~"- Vetores Ordem Decrescente")
+
+#dados = subset(dados, Condição == 'ordem-crescente')
 #nome = bquote(.(nomeFile)~"- Vetores Ordem Crescente")
 
-plot(dados$`Tamanho do vetor`, dados$`Tempo de execuï¿½ï¿½o (microsec)`, xlab = "Tamanho do vetor", ylab = "Tempo de execuï¿½ï¿½o (microsegundos)", main = nome)
-plot(dados$`Tamanho do vetor`, dados$`Trocas realizadas`, xlab = "Tamanho do vetor", ylab = "Nï¿½mero de trocas", main = nome)
-plot(dados$`Tamanho do vetor`, dados$`Comparaï¿½ï¿½es realizadas`, xlab = "Tamanho do vetor", ylab = "Nï¿½mero de comparaï¿½ï¿½es", main = nome)
+#plot(dados$`Tamanho do vetor`, dados$`Tempo de execução (microsec)`, xlab = "Tamanho do vetor", ylab = "Tempo de execuï¿½ï¿½o (microsegundos)", main = nome)
+#plot(dados$`Tamanho do vetor`, dados$`Trocas realizadas`, xlab = "Tamanho do vetor", ylab = "Nï¿½mero de trocas", main = nome)
+#plot(dados$`Tamanho do vetor`, dados$`Comparaï¿½ï¿½es realizadas`, xlab = "Tamanho do vetor", ylab = "Nï¿½mero de comparaï¿½ï¿½es", main = nome)
 
 ##TEMPOS DE EXECUï¿½ï¿½O
 library(dplyr)
 graph<- aggregate(dados$'Tempo total', list(dados$`Tamanho do vetor`), FUN = mean)
-#plot das mï¿½dias
-plot(graph, xlab = "Tamanho do vetor", ylab = "Tempo de execuï¿½ï¿½o (microsegundos)", main = bquote(.(nome) ~"- Mï¿½dias"))
+
+#plot das médias
+plot(graph, xlab = "Tamanho do vetor", ylab = "Tempo de execução (microsegundos)", main = bquote(.(nome) ~"- Médias"))
 
 
-plot(graph, xlab = "Tamanho do vetor", ylab = "Tempo de execuï¿½ï¿½o (microsegundos)", main = bquote(.(nome) ~""), pch = 18, col = "gray",xlim=c(0,10000),ylim=c(0,60000))
+plot(graph, xlab = "Tamanho do vetor", ylab = "Tempo de execução (microsegundos)", main = bquote(.(nome) ~""), pch = 18, col = "gray",xlim=c(0,10000),ylim=c(0,60000))
 
-##MUDAR FUNï¿½ï¿½O pior caso
+##MUDAR Funções pior caso
 par(new = TRUE)
 curve(x^2, from = 0, to = 10000, axes = FALSE, col = "red", ylab = "", xlab = "",lwd=2,xlim=c(0,10000),ylim=c(0,60000))
 
-##MUDAR FUNï¿½ï¿½O melhor CASO
+##MUDAR Função melhor CASO
 par(new = TRUE)
 curve(x^1, from = 0, to = 10000, axes = FALSE,lwd=2, ylab = "", xlab = "" ,col = "green",xlim=c(0,10000),ylim=c(0,60000))
